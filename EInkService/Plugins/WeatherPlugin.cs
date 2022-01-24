80642dc08acfbd70ca3f95e6eb787f3f89c71ce5
Tharnas <8459274+Tharnas@users.noreply.github.com>
2022-01-24 18:44:18 +0100

    Split current and daily weather into separate methods

--- EInkService/Plugins/WeatherPlugin.cs
+++ EInkService/Plugins/WeatherPlugin.cs
@@ -4,6 +4,7 @@ using EInkService.Options;
 using Microsoft.Extensions.Options;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.Linq;
@@ -27,67 +28,62 @@ namespace EInkService.Plugins
         {
             var result = await _openWeatherMapService.GetCurrentWeather(_options.Value.Latitude, _options.Value.Longitude);
 
-            var sunrise = result.current.sunrise;
-            var sunset = result.current.sunset;
-            //var rain3h = result.rain.rain.1h;
-            //var snow3h = result.snow.1h;
-
-            var windSpeedInKmH = result.current.wind_speed * 3.6;
-            var windDirection = result.current.wind_deg;
+            using var currentWeatherImage = new Image<Rgba32>(width, height / 3);
+            RenderCurrentWeather(currentWeatherImage, theme, result);
+            image.Mutate(x => x.DrawImage(currentWeatherImage, new Point(0, 0), 1));
 
 
+            using var dailyWeatherImage = new Image<Rgba32>(width, height / 3);
+            RenderDailyWeather(dailyWeatherImage, theme, result);
+            image.Mutate(x => x.DrawImage(dailyWeatherImage, new Point(0, height * 2 / 3), 1));
+        }
+
+        private static void RenderDailyWeather(Image image, Theme theme, GetOneCallApiResult result)
+        {
+            var dayWidth = image.Width / result.daily.Length;
+
+            for (int i = 0; i < result.daily.Length; i++)
+            {
+                var currentDailyTop = image.Height;
+                var dailyDaySize = image.DrawString(result.daily[i].dt.ToString("ddd"), theme.RegularText, theme.PrimaryColor, new Point(i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
+                currentDailyTop -= (int)dailyDaySize.Height + theme.Margin;
+
+                var dailyIconSize = image.DrawString(GetIconFont(result.daily[i].weather.First().icon), theme.DailyWeatherIconFont, theme.PrimaryColor, new Point(i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
+                currentDailyTop -= (int)(dailyIconSize.Height + theme.Margin);
+
+                var dailyMinTemperatureSize = image.DrawString($"{result.daily[i].temp.min:0.0}°", theme.RegularText, theme.PrimaryColor, new Point(i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
+                currentDailyTop -= (int)dailyMinTemperatureSize.Height + 2;
+
+                image.DrawString($"{result.daily[i].temp.max:0.0}°", theme.RegularText, theme.PrimaryColor, new Point(i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
+            }
+        }
+
+        private static void RenderCurrentWeather(Image image, Theme theme, GetOneCallApiResult result)
+        {
             var weather = result.current.weather.First();
-            var temp = result.current.temp;
-            var tempFeelsLike = result.current.feels_like;
-            var humidity = result.current.humidity;
 
-            var weatherTodayHeight = height / 3;
-            var weatherTodayWidth = width;
-            //using var icon = await _openWeatherMapService.GetIcon(weather.icon);
-
-            var temperatureText = $"{temp:0.0}°C";
+            var temperatureText = $"{result.current.temp:0.0}°";
             var temperatureMesurment = TextMeasurer.Measure(temperatureText, new RendererOptions(theme.TemperatureText));
             var iconMesurment = TextMeasurer.Measure(GetIconFont(weather.icon), new RendererOptions(theme.WeatherIconFont));
 
             var iconAndTemperatureWidth = temperatureMesurment.Width + iconMesurment.Width + theme.Margin;
             var iconAndTemperatureHeight = Math.Max(temperatureMesurment.Height, iconMesurment.Height);
 
-            image.DrawString(temperatureText, theme.TemperatureText, theme.PrimaryColor, new Point((int)((weatherTodayWidth / 2) - (iconAndTemperatureWidth / 2)), theme.Margin));
-            image.DrawString($"{tempFeelsLike}°C", theme.Subline, theme.PrimaryColor, new Point((int)((weatherTodayWidth / 2) - (iconAndTemperatureWidth / 2) + temperatureMesurment.Width), (int)iconAndTemperatureHeight), AlignEnum.End, AlignEnum.End);
-            image.DrawString(GetIconFont(weather.icon), theme.WeatherIconFont, theme.PrimaryColor, new Point((int)((weatherTodayWidth / 2) - (iconAndTemperatureWidth / 2) + temperatureMesurment.Width + theme.Margin), 0));
-            image.DrawString(weather.description, theme.Subline, theme.PrimaryColor, new Point(weatherTodayWidth / 2, (int)iconAndTemperatureHeight + theme.Margin), AlignEnum.Center);
+            image.DrawString(temperatureText, theme.TemperatureText, theme.PrimaryColor, new Point((int)((image.Width / 2) - (iconAndTemperatureWidth / 2)), theme.Margin));
+            image.DrawString($"{result.current.feels_like:0.0}°", theme.Subline, theme.PrimaryColor, new Point((int)((image.Width / 2) - (iconAndTemperatureWidth / 2) + temperatureMesurment.Width), (int)iconAndTemperatureHeight), AlignEnum.End, AlignEnum.End);
+            image.DrawString(GetIconFont(weather.icon), theme.WeatherIconFont, theme.PrimaryColor, new Point((int)((image.Width / 2) - (iconAndTemperatureWidth / 2) + temperatureMesurment.Width + theme.Margin), 0));
+            image.DrawString(weather.description, theme.Subline, theme.PrimaryColor, new Point(image.Width / 2, (int)iconAndTemperatureHeight + theme.Margin), AlignEnum.Center);
 
-            image.DrawString(sunrise.ToString("t"), theme.SmallText, theme.PrimaryColor, new Point(theme.Margin, theme.Margin));
-            image.DrawString(sunset.ToString("t"), theme.SmallText, theme.PrimaryColor, new Point(weatherTodayWidth, theme.Margin), AlignEnum.End);
+            image.DrawString(result.current.sunrise.ToString("t"), theme.SmallText, theme.PrimaryColor, new Point(theme.Margin, theme.Margin));
+            image.DrawString(result.current.sunset.ToString("t"), theme.SmallText, theme.PrimaryColor, new Point(image.Width, theme.Margin), AlignEnum.End);
 
-            var windSpeedSize = image.DrawString($"{windSpeedInKmH.ToString("0.0")} km/h", theme.SmallText, theme.PrimaryColor, new Point(weatherTodayWidth, weatherTodayHeight), AlignEnum.End, AlignEnum.End);
-            image.DrawString($"{windDirection.ToString("0.0")}°", theme.SmallText, theme.PrimaryColor, new Point(weatherTodayWidth, (int)(weatherTodayHeight - windSpeedSize.Height)), AlignEnum.End, AlignEnum.End);
-
-
-
-            // daily
-            var dailyHeight = height / 3;
-            var dailyWidth = width;
-            var dailyTop = height * 2 / 3;
-            var dailyLeft = 0;
-
-            var dayWidth = dailyWidth / result.daily.Length;
-
-            for (int i = 0; i < result.daily.Length; i++)
-            {
-                var currentDailyTop = dailyTop + dailyHeight;
-                var dailyDaySize = image.DrawString(result.daily[i].dt.ToString("ddd"), theme.RegularText, theme.PrimaryColor, new Point(dailyLeft + i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
-                currentDailyTop -= (int)dailyDaySize.Height + theme.Margin;
-
-                var dailyIconSize = image.DrawString(GetIconFont(result.daily[i].weather.First().icon), theme.DailyWeatherIconFont, theme.PrimaryColor, new Point(dailyLeft + i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
-                currentDailyTop -= (int)(dailyIconSize.Height + theme.Margin);
-
-                var dailyMinTemperatureSize = image.DrawString($"{result.daily[i].temp.min:0.0}°", theme.RegularText, theme.PrimaryColor, new Point(dailyLeft + i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
-                currentDailyTop -= (int)dailyMinTemperatureSize.Height + 2;
-
-                image.DrawString($"{result.daily[i].temp.max:0.0}°", theme.RegularText, theme.PrimaryColor, new Point(dailyLeft + i * dayWidth + dayWidth / 2, currentDailyTop), AlignEnum.Center, AlignEnum.End);
-            }
+            var windSpeedInKmH = result.current.wind_speed * 3.6;
+            var windDirection = result.current.wind_deg;
+            var windSpeedSize = image.DrawString($"{windSpeedInKmH.ToString("0.0")} km/h", theme.SmallText, theme.PrimaryColor, new Point(image.Width, image.Height), AlignEnum.End, AlignEnum.End);
+            image.DrawString($"{windDirection.ToString("0.0")}°", theme.SmallText, theme.PrimaryColor, new Point(image.Width, (int)(image.Height - windSpeedSize.Height)), AlignEnum.End, AlignEnum.End);
         }
+
+
 
         private static string GetIconFont(string icon)
         {
